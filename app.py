59b3bf0c51924738997c5f5f0c52d32735a13fb0
Ashiq <ashiq.inform@gmail.com>
2020-01-06 20:31:23 -0500

    simple changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import datetime
-import pytz # timezone 
+import pytz # timezone
 import requests
 import os
 
@@ -8,11 +8,11 @@ import os
 
 app = Flask(__name__)
 
-
+# This mainly code directs the person to the home page of the Website.
 @app.route('/', methods=['GET'])
 def home_page():
 	return render_template('index.html')
-
+# if some types an extra /ashiq... this codes adds it to the website intro
 @app.route('/<name>')
 def profile(name):
 	return render_template('index.html', name=name)
@@ -44,20 +44,20 @@ def shopping_list_post():
       return render_template('shopping_list.html')
     elif request.method == 'POST':
           print(request.form['text'].split())
-          
+
           shop_list = []
           try:
             for item in request.form['text'].split():
-              
+
               shop_list.append(item)
 
-              
-              
+
+
             return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
           except ValueError:
             return "Easy now! Let's keep it simple! Just words with a space between them"
-          
-  	      
+
+
 @app.route('/time', methods=['GET','POST'])
 def time_post():
     # --> ['5', '6', '8']
@@ -67,17 +67,17 @@ def time_post():
       return render_template('time.html')
     elif request.method == 'POST':
           print(request.form['text'].split())
-          
+
           for item in request.form['text'].split():
             answer = (datetime.datetime.now(pytz.timezone("Europe/Dublin")).strftime('Time = ' + '%H:%M:%S' + ' GMT ' + ' Year = ' + '%d-%m-%Y'))
             #answer = datetime.datetime.now().strftime('Time == ' + '%H:%M:%S' + ' Year == ' + '%d-%m-%Y')
             #answer = datetime.datetime.now().strftime('%Y-%m-%d \n %H:%M:%S')
 
-              
-              
+
+
             return render_template('time.html', result=answer)
 
-         
+
 
 @app.route('/python_apps')
 def python_apps_page():
